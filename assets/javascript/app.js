d763024da93fd67ae274ebc80d88ffae3931390f
JesseL <35356853+loftusjl@users.noreply.github.com>
2018-06-11 15:10:05 -0700

    initial JS setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+// VARIABLES
+var timer = 0;
+var numCorrect = 0;
+var numIncorrect = 0;
+var numUnanswered = 0;
+var correctAnswer = '';
+var incorrectAnswer = '';
+var question = '';
+
+// BODY
+window.onload = function () {
+    
+};
+
+// FUNCTIONS
+function countdownTimer() {
+
+};
+function initializeGame() {
+
+};
+function submitAnswer() {
+
+};
+function randomizeQuestions() {
+
+};
+function randomizeAnswers() {
+
+};
